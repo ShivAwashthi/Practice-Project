0e2b5907d29cd298bfb70475fb07138b457fc220
Shiv Awashthi <awashthishiv2002@gmail.com>
2026-04-17 11:22:19 +0530

    Intial commit

---
+++ backend/Practice-Lost/src/main/java/com/example/Practice_Lost/PracticeLostApplication.java
@@ -0,0 +1,13 @@
+package com.example.Practice_Lost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeLostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeLostApplication.class, args);
+	}
+
+}
---
+++ backend/Practice-Lost/src/test/java/com/example/Practice_Lost/PracticeLostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Practice_Lost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeLostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
